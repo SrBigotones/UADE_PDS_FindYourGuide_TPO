4ee90e3d67bba6b458a650992f9ae3dac93bec46
Nicolas Repollo <nicolasrepollo2000@hotmail.com>
2024-06-12 21:30:13 -0300

    Commit inicial de proyecto Spring

---
+++ findyourguide/src/test/java/com/uade/pds/findyourguide/FindYourGuideApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uade.pds.findyourguide;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FindYourGuideApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
