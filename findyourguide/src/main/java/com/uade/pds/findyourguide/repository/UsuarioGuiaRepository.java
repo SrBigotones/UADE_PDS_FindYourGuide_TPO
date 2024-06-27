ea11e49964314ec9530045be5bd67894dd804c80
freefrozztea <luisfalmi@gmail.com>
2024-06-27 19:46:39 -0300

    delete unnecessary imports

--- findyourguide/src/main/java/com/uade/pds/findyourguide/repository/UsuarioGuiaRepository.java
+++ findyourguide/src/main/java/com/uade/pds/findyourguide/repository/UsuarioGuiaRepository.java
@@ -2,10 +2,8 @@ package com.uade.pds.findyourguide.repository;
 
 import com.uade.pds.findyourguide.model.user.UsuarioGuia;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface UsuarioGuiaRepository extends JpaRepository<UsuarioGuia, Long> {
